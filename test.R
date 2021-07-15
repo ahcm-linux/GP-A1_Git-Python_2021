c26cc4bacfba1bcce60e94da4a8e40ea67376af8
Alexandre Marques <ahcm.linux@gmail.com>
2021-07-15 01:58:12 -0300

    quinto commit

--- test.R
+++ test.R
@@ -1,3 +1,5 @@
+# code in R
+
 x <- c(0, 2, 1)
 y <- c(0, 0, sqrt(3))
 
@@ -7,9 +9,9 @@ ry <- 0.6
 plot(x, y, type = "p", cex = 1.5, pch = 20, col = "red")
 points(rx, ry, cex = 1.5, pch = 20, col = "red")
 
-N <- 3000
-for (i in 1:N) {
-  selection <- sample(1:length(x), 1)
+n <- 3000
+for (i in 1:n) {
+  selection <- sample(seq_len(length(x)), 1)
   rx <- (rx + x[selection]) / 2
   ry <- (ry + y[selection]) / 2
   points(rx, ry, cex = 0.5, pch = 20)
